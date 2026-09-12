6d747f3f8edead7f9660a3834ca42b4c34a4b425
Vaani-bisht <vaanibisht18@gmail.com>
2026-09-12 11:23:20 +0530

    Time: 47 ms (35.04%), Space: 75.9 MB (7.65%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+
+    ListNode* mergeList(ListNode* list1 , ListNode* list2){
+        ListNode* dummy = new ListNode(0);
+        ListNode* temp = dummy;
+
+        while(list1 != NULL && list2 != NULL){
+
+            if(list1->val > list2->val){
+                temp->next = list2;
+                list2 = list2->next;
+            }else{
+                temp->next = list1;
+                list1 = list1->next;
+            }
+            temp = temp->next;
+        }
+        if(list1 != NULL){
+            temp->next = list1;
+        }
+        if(list2 != NULL){
+            temp->next = list2;
+        }
+        return dummy->next;
+    }
+
+    ListNode* sortList(ListNode* head) {
+        if(head == NULL || head->next == NULL){
+            return head;
+        }
+
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* second = slow->next;
+        slow->next = NULL;
+
+        ListNode* first = sortList(head);
+        second = sortList(second);
+
+        return mergeList(first , second);
+    }
+};
